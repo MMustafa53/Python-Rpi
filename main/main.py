6ecfb39b848ee72aeb53a7b8eade9929e8bb1974
Muhammed Mustafa Hüsrevoğlu <muhammed.husrevoglu@prodabilisim.com>
2020-06-07 17:20:07 +0300

    frontend completed

--- main/main.py
+++ main/main.py
@@ -1,5 +1,6 @@
-from guizero import Box, App, Text, TextBox, PushButton, Slider, Combo, CheckBox, ButtonGroup, info, Window
+from guizero import *
 
+sure = 15
 cevaplar = {"s1_cevap": "",
             "s2_cevap": "",
             "s3_cevap": "",
@@ -37,10 +38,64 @@ def geri_bes():
     sb4window.show()
 
 
+def ileri_view():
+    print(cevaplar)
+    app.hide()
+    ileri_window.show()
+
+
+def iptal_view():
+    print(cevaplar)
+    app.show()
+    ileri_window.hide()
+
+
+def islem_view():
+    print('islem yapıldı')
+    islem_text_sure.value = sure
+    islem_text_sure.visible = True
+    islem_text_sure.repeat(1000, count_islem)
+    ileri_window.hide()
+    islem_window.show()
+
+
+def acil_islem():
+    print('acil durum')
+    islem_text_sure.value = int(islem_text_sure.value) + 10
+
+
+def onay_view():
+    onay_buton.enabled = False
+    iptal_buton.enabled = False
+    onay_sn_text.visible = True
+    saniye = 2
+    saniye_text.value = saniye
+    saniye_text.repeat(1000, count_down)
+    saniye_text.after(2000, islem_view)
+    saniye_text.visible = True
+    print(saniye)
+    print(cevaplar)
+
+
+def count_down():
+    if int(saniye_text.value) > 0:
+        saniye_text.value = int(saniye_text.value) - 1
+
+
+def count_islem():
+    if int(islem_text_sure.value) > 0:
+        islem_text_sure.value = int(islem_text_sure.value) - 1
+    else:
+        islem_window.hide()
+        islem_son_window.show()
+        islem_son_text.visible = True
+
+
+
 def manuel_sterilizasyon_view():
     print(cevaplar)
-    sb1window.show()
     app.hide()
+    sb1window.show()
 
 
 def close_window_bir_e():
@@ -101,6 +156,7 @@ def close_window_bes():
     str = ', \n'.join(list(cevaplar.values()))
     cevap_text.append(str)
     cevap_text.visible = True
+    ileri_buton.visible = True
     app.show()
 
 
@@ -110,9 +166,26 @@ welcome_message = Text(app, text="Sterilizasyon Şeklini şeçiniz.\n", grid=[0,
 manuel_buton = PushButton(app, text="Manuel Sterilizasyon", command=manuel_sterilizasyon_view, grid=[0, 1])
 oto_buton = PushButton(app, text="Otomatik Sterizlizasyon", command=manuel_sterilizasyon_view, grid=[1, 1],
                        enabled=False)
-str = '\n\n'
-cevap_text = Text(app, text=str, grid=[0, 2], visible=False, align='left')
 
+islem_son_window = Window(app, title="Sterilizasyon İşlemi Tamamlandı", visible=False)
+islem_son_text = Text(islem_son_window, text="\n\nSterilizasyon işlemi tamamlandı.\n Havalandırma sistemi durduğunda cihazı kapatabilirsiniz.", visible=False)
+
+islem_window = Window(app, title="Sterilizasyon Yapılıyor", visible=False)
+islem_text = Text(islem_window, text="İşlem Süresi- Saniye")
+islem_text_sure = Text(islem_window, text="", visible=False)
+acil_buton = PushButton(islem_window, text="Acil Durum", command=acil_islem)
+
+ileri_window = Window(app, title="Onaylıyor Musunuz?", visible=False)
+ileri_text = Text(ileri_window, text="Seçtiğiniz kriterlere göre sterilizasyon işlemi 1 dakika sürecektir.\nOnaylıyor musunuz?\n")
+onay_buton = PushButton(ileri_window, text="Onayla", command=onay_view)
+iptal_buton = PushButton(ileri_window, text="İptal", command=iptal_view)
+onay_sn_text = Text(ileri_window, text="\nSterilizasyon işlemi 60 sn içinde başlayacaktır.\n", visible=False)
+saniye_text = Text(ileri_window, text="", visible=False)
+
+str = '\n\n'
+
+cevap_text = Text(app, text=str, grid=[0, 2], visible=False, align='left')
+ileri_buton = PushButton(app, text="İleri", command=ileri_view, grid=[0, 8], visible=False)
 sb1window = Window(app, title="Soru 1", visible=False)
 
 soru_bir = Text(sb1window, text="Sterilizasyon esnasında ortamda insan/hayvan bulunacak mı?\n")
