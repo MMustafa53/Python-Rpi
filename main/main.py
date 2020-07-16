cd974ea7f697a4afb30f30ccb2aba65371d7801c
Husrevoglu <hsurevoglu61@gmail.com>
2020-07-16 23:28:14 +0300

    push push push

--- main/main.py
+++ main/main.py
@@ -1,51 +1,54 @@
 from guizero import *
+from gpiozero import LED
 
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(2, GPIO.OUT)
+pwm = GPIO.PWM(2, 100)
+pwm.start(0)
+freq = 0
 sure = 15
 cevaplar = {"s1_cevap": "",
             "s2_cevap": "",
             "s3_cevap": "",
             "s4_cevap": "",
             "s5_cevap": ""}
-
+led = LED(17)
 
 def geri_bir():
-    print(cevaplar)
     sb1window.hide()
     app.show()
 
 
 def geri_iki():
-    print(cevaplar)
     sb2window.hide()
     sb1window.show()
 
 
 def geri_uc():
-    print(cevaplar)
     sb3window.hide()
     sb2window.show()
 
 
 def geri_dort():
-    print(cevaplar)
     sb4window.hide()
     sb3window.show()
 
 
 def geri_bes():
-    print(cevaplar)
     sb5window.hide()
     sb4window.show()
 
 
 def ileri_view():
-    print(cevaplar)
     app.hide()
     ileri_window.show()
 
 
 def iptal_view():
-    print(cevaplar)
     app.show()
     ileri_window.hide()
 
@@ -94,6 +97,10 @@ def count_islem():
         islem_son_window.show()
         islem_son_text.visible = True
 
+
+def sterilizasyon_view():
+    app.hide()
+    oto_window.show()
 
 
 def manuel_sterilizasyon_view():
@@ -164,31 +171,68 @@ def close_window_bes():
     app.show()
 
 
+def st_oto_open():
+    led.on()
+    print("LED ON")
+
+
+def st_oto_close():
+    led.off()
+    print("LED OFF")
+
+
+def st_oto_back():
+    oto_window.hide()
+    app.show()
+
+
+def set_pwm(freq):
+    a = oto_son_text.value
+    if int(a) >= 100:
+        freq = 0
+        a = 0
+    freq = 20 + int(a)
+    oto_son_text.value = freq
+    print("PWM: ", freq)
+    pwm.changeDutyCycle(freq)
+
+
 app = App(title="Ana Sayfa", layout="grid")
-app.set_full_screen()
+# app.set_full_screen()
 app.when_closed = stop_app
 
 welcome_message = Text(app, text="Sterilizasyon Şeklini şeçiniz.\n", grid=[0, 0])
 manuel_buton = PushButton(app, text="Manuel Sterilizasyon", command=manuel_sterilizasyon_view, grid=[0, 1])
-oto_buton = PushButton(app, text="Otomatik Sterizlizasyon", command=manuel_sterilizasyon_view, grid=[1, 1],
-                       enabled=False)
+oto_buton = PushButton(app, text="Sterizlizasyon", command=sterilizasyon_view, grid=[1, 1])
+
+oto_window = Window(app, title="Sterilizasyon", visible=False)
+oto_window.when_closed = stop_app
+# oto_window.set_full_screen()
+
+oto_open = PushButton(oto_window, text="Aç", command=st_oto_open)
+oto_close = PushButton(oto_window, text="Kapat", command=st_oto_close)
+oto_back = PushButton(oto_window, text="PWM +20", command=set_pwm, args=[freq])
+oto_son_text = Text(oto_window, text= str(freq))
 
 islem_son_window = Window(app, title="Sterilizasyon İşlemi Tamamlandı", visible=False)
 islem_son_window.when_closed = stop_app
-islem_son_window.set_full_screen()
-islem_son_text = Text(islem_son_window, text="\n\nSterilizasyon işlemi tamamlandı.\n Havalandırma sistemi durduğunda cihazı kapatabilirsiniz.", visible=False)
+# islem_son_window.set_full_screen()
+islem_son_text = Text(islem_son_window,
+                      text="\n\nSterilizasyon işlemi tamamlandı.\n Havalandırma sistemi durduğunda cihazı kapatabilirsiniz.",
+                      visible=False)
 
 islem_window = Window(app, title="Sterilizasyon Yapılıyor", visible=False)
 islem_window.when_closed = stop_app
-islem_window.set_full_screen()
+# islem_window.set_full_screen()
 islem_text = Text(islem_window, text="İşlem Süresi- Saniye")
 islem_text_sure = Text(islem_window, text="", visible=False)
 acil_buton = PushButton(islem_window, text="Acil Durum", command=acil_islem)
 
 ileri_window = Window(app, title="Onaylıyor Musunuz?", visible=False)
 ileri_window.when_closed = stop_app
-ileri_window.set_full_screen()
-ileri_text = Text(ileri_window, text="Seçtiğiniz kriterlere göre sterilizasyon işlemi 1 dakika sürecektir.\nOnaylıyor musunuz?\n")
+# ileri_window.set_full_screen()
+ileri_text = Text(ileri_window,
+                  text="Seçtiğiniz kriterlere göre sterilizasyon işlemi 1 dakika sürecektir.\nOnaylıyor musunuz?\n")
 onay_buton = PushButton(ileri_window, text="Onayla", command=onay_view)
 iptal_buton = PushButton(ileri_window, text="İptal", command=iptal_view)
 onay_sn_text = Text(ileri_window, text="\nSterilizasyon işlemi 60 sn içinde başlayacaktır.\n", visible=False)
@@ -201,7 +245,7 @@ ileri_buton = PushButton(app, text="İleri", command=ileri_view, grid=[0, 8], vi
 
 sb1window = Window(app, title="Soru 1", visible=False)
 sb1window.when_closed = stop_app
-sb1window.set_full_screen()
+# sb1window.set_full_screen()
 
 soru_bir = Text(sb1window, text="Sterilizasyon esnasında ortamda insan/hayvan bulunacak mı?\n")
 evet_bir = PushButton(sb1window, text="Evet ", command=close_window_bir_e)
@@ -210,7 +254,7 @@ geri_bir = PushButton(sb1window, text="Geri Dön", command=geri_bir)
 
 sb2window = Window(app, title="Soru 2", visible=False)
 sb2window.when_closed = stop_app
-sb2window.set_full_screen()
+# sb2window.set_full_screen()
 
 soru_iki = Text(sb2window, text="Sterilizasyon yapılacak yerde ozona/paslanmaya\n dayanıksız malzeme var mı?\n")
 evet_iki = PushButton(sb2window, text="Evet ", command=close_window_iki_e)
@@ -219,7 +263,7 @@ geri_iki = PushButton(sb2window, text="Geri Dön", command=geri_iki)
 
 sb3window = Window(app, title="Soru 3", visible=False)
 sb3window.when_closed = stop_app
-sb3window.set_full_screen()
+# sb3window.set_full_screen()
 soru_uc = Text(sb3window, text="Sterilizasyon yapılacak yerde hava akımı var mı?\n")
 evet_uc = PushButton(sb3window, text="Evet ", command=close_window_uc_e)
 hayir_uc = PushButton(sb3window, text="Hayır", command=close_window_uc_h)
@@ -227,7 +271,7 @@ geri_uc = PushButton(sb3window, text="Geri Dön", command=geri_uc)
 
 sb4window = Window(app, title="Soru 4", visible=False)
 sb4window.when_closed = stop_app
-sb4window.set_full_screen()
+# sb4window.set_full_screen()
 soru_dort = Text(sb4window, text="Sterilizasyon yapılacak yerin alanı - m\u00b2 \n")
 alan = TextBox(sb4window, text="50")
 alan_kaydet = PushButton(sb4window, text="Kaydet", command=close_window_dort)
@@ -235,7 +279,7 @@ geri_dort = PushButton(sb4window, text="Geri Dön", command=geri_dort)
 
 sb5window = Window(app, title="Soru 5", visible=False)
 sb5window.when_closed = stop_app
-sb5window.set_full_screen()
+# sb5window.set_full_screen()
 soru_bes = Text(sb5window, text="Sterilizasyon yapılacak yerin yüksekliği - m\n")
 yukseklik = TextBox(sb5window, text="50")
 yukseklik_kaydet = PushButton(sb5window, text="Kaydet ", command=close_window_bes)
